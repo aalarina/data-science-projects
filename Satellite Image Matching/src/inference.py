95db979f0b0c1d0ff5ffd7c056279c09e60c76b1
aalarina <147263709+aalarina@users.noreply.github.com>
2026-07-15 14:07:54 +0300

    Update inference.py

--- Satellite Image Matching/src/inference.py
+++ Satellite Image Matching/src/inference.py
@@ -1,1 +1,27 @@
+mkpts0, mkpts1, confidence = match_all_tiles(
+    tilesA,
+    tilesB,
+    matcher,
+    device
+)
+
+mask = confidence > 0.8
+
+mkpts0 = mkpts0[mask]
+mkpts1 = mkpts1[mask]
+confidence = confidence[mask]
+
+print(len(confidence))
+print(confidence.mean())
+print(confidence.max())
+
+idx = confidence.argsort()[::-1]
+
+mkpts0 = mkpts0[idx]
+mkpts1 = mkpts1[idx]
+confidence = confidence[idx]
+
+mkpts0 = mkpts0[:100]
+mkpts1 = mkpts1[:100]
+
 
